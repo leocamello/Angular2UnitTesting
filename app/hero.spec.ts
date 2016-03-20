0fd0c448f529c98a9e15294e09c8fbe35f36901a
Leonardo Nascimento <leocamello@gmail.com>
2016-03-19 23:46:43 -0300

    Angular 2 Unit Testing App

---
+++ app/hero.spec.ts
@@ -0,0 +1,11 @@
+import {Hero} from './hero';
+describe('Hero', () => {
+  it('has name given in the constructor', () => {
+    let hero = new Hero(1, 'Super Cat');
+    expect(hero.name).toEqual('Super Cat');
+  });
+  it('has id given in the constructor', () => {
+    let hero = new Hero(1, 'Super Cat');
+    expect(hero.id).toEqual(1);
+  });
+});
